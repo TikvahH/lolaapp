a9c9bd0f1c8ce81d860c538cce75b5b14ca445b4
Tikvah <T.harr@live.com>
2015-06-09 11:41:10 -0400

    Confirgered for bootstrap SASS

--- config/application.rb
+++ config/application.rb
@@ -1,15 +1,5 @@
 require File.expand_path('../boot', __FILE__)
 
-module LolaApp
-  class Application < Rails::Application
-
-    config.generators do |g|
-      g.orm             :active_record
-      g.template_engine :erb
-    end
-
-
-
 require "rails"
 # Pick the frameworks you want:
 require "active_model/railtie"
@@ -27,6 +17,11 @@ Bundler.require(*Rails.groups)
 
 module LolaAppAuth
   class Application < Rails::Application
+
+    config.generators do |g|
+      g.orm             :active_record
+      g.template_engine :erb
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
